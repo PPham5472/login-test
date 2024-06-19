531ffc13dc5c2db8c81b452a7788dbc4b5a9ef85
Peter Pham <ppham5472@gmail.com>
2024-06-18 22:52:59 -0600

    Login Page Starter

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import Illu from "./assets/Illu.png";
+import Logo from "./assets/Logo.png";
+import Mail from "./assets/Mail.png";
+import "./styles.css";
 
 const Home = () => {
-    return <div>Home</div>;
+    return (
+        <div className="App">
+            <header className="header">
+                <img src={Logo} />
+            </header>
+            <main className="main">
+                <img className="illustration" src={Illu} />
+                <section className="section-login">
+                    <label className="input-label">
+                        <img className={"mail"} src={Mail} />
+                        Email address
+                    </label>
+                    <input className="input" placeholder="jordan@gmail.com" />
+                    <label className="input-label">Password</label>
+                    <input className="input" placeholder="Applause123$" />
+                </section>
+            </main>
+        </div>
+    );
 };
 
 Home.defaultProps = {};
