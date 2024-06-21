49e8083c5bcf179c7f473cc552b1da3ee6344632
Peter Pham <ppham5472@gmail.com>
2024-06-20 23:19:54 -0600

    Update Express

--- netlify/functions/api/index.js
+++ netlify/functions/api/index.js
@@ -1,15 +1,26 @@
 import express, { Router } from "express";
 import serverless from "serverless-http";
 
+const users = [
+    { email: "test@applausehq.com", password: "test1234!" },
+    { email: "peter@applausehq.com", password: "Cookies123!" },
+];
+
 const api = express();
 const router = Router();
 api.use("/api/", router);
 
 router.post("/login", (req, res) => {
     const { body } = req;
-    console.log("test", body);
-    res.send("Test");
+
+    const currentUser = users.filter((user) => user.email === body.email)[0];
+    if (!currentUser) return res.status(400).json({ body, status: "failed", error: "Invalid Login" });
+
+    if (currentUser?.password === body.password) {
+        return res.status(200).json({ body, status: "success", userEmail: currentUser?.email });
+    }
+
+    return res.status(400).json({ body, status: "failed", error: "Invalid Login 2" });
 });
-router.get("/hello", (req, res) => res.send("Hello World!"));
 
 export const handler = serverless(api);
